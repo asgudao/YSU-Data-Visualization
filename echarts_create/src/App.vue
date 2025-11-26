750741b83ff3bce468a7ed9acbf5d77bc0917552
asgudao <3418840632@qq.com>
2025-11-26 15:41:18 +0800

    模板

---
+++ echarts_create/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import * as echarts from 'echarts';
+import {onMounted} from "vue";
+
+onMounted(() => {
+  var chartDom = document.getElementById('main');
+  var myChart = echarts.init(chartDom);
+  var option;
+
+  option = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        smooth: true
+      }
+    ]
+  };
+
+  option && myChart.setOption(option);
+});
+</script>
+
+<template>
+  <div id="main" style="width: 800px;height: 300px"></div>
+</template>
+
+<style scoped>
+
+</style>
